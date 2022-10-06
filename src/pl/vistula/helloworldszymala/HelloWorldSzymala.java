e3cc086e3f2ad562b017d8d877309fefff7e79c1
Adrian Szymala <aszymal1@stu.vistula.edu.pl>
2022-10-06 18:27:24 +0200

    Commit 4

--- src/pl/vistula/helloworldszymala/HelloWorldSzymala.java
+++ src/pl/vistula/helloworldszymala/HelloWorldSzymala.java
@@ -3,8 +3,7 @@ package pl.vistula.helloworldszymala;
 public class HelloWorldSzymala {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Szymala");
-        System.out.println("Hello");
-        System.out.println("Hi");
-
+        System.out.println("Hello Vistula Szymala");
+        System.out.println("Hi Vistula Szymala");
     }
 }
